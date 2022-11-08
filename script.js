2eacba324f464e053bef8bc31f6d50c77e2b52f4
cadienlake <115520010+cadienlake@users.noreply.github.com>
2022-11-07 21:41:13 -0600

    functioning great, just needs a readme

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ let allChoices = [];
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
-``
 // Write password to the #password input
 
 function generatePassword() {
+  // password is an empty array
+  password = [];
   // Ask user how long the password should be
-  let passwordLength = window.prompt("Please choose a number between 8-20 for your password length.");
+  let passwordLength = window.prompt("Please choose a number between 4-20 for your password length.");
   // TODO: Validate that they entered a number between 8-20
+  if (passwordLength < 4 || passwordLength > 20 || isNaN(passwordLength)) {
+    window.alert("You must chose a number between 4 and 20.");
+    return null;
+  }
   // Ask user if they want numbers
   let isNumbers = window.confirm("Do you want to include numbers?");
   // ask user if they want upper case letters
@@ -24,24 +29,42 @@ function generatePassword() {
   let isLower = window.confirm("Do you want to include lower case letters?");
   // ask uers if they want special characters
   let isSpecial = window.confirm("Do you want to include special characters?");
-  // password is an empty array
-  password = [];
+  // Check that they've selected at least one input
+  if (isNumbers === false && isUpper === false && isLower === false && isSpecial === false) {
+    window.alert("You must chose at least one character type.");
+    return null;
+  }
   // loops through the index of 
   if (isNumbers === true) {
-    // TODO: pick one random number and push it into password
     allChoices = allChoices.concat(number);
+    let maximum = number.length - 1;
+    let rnd = Math.floor(Math.random() * (maximum - 0 + 1)) + 0;
+    password.push(number[rnd]);
+    passwordLength--;
   }
-  if (isUpper === true) {
+  if (isUpper === true) {``
     allChoices = allChoices.concat(upper);
+    let maximum = upper.length - 1;
+    let rnd = Math.floor(Math.random() * (maximum - 0 + 1)) + 0;
+    password.push(upper[rnd]);
+    passwordLength--;
   }
   if (isLower === true) {
     allChoices = allChoices.concat(lower);
+    let maximum = lower.length - 1;
+    let rnd = Math.floor(Math.random() * (maximum - 0 + 1)) + 0;
+    password.push(lower[rnd]);
+    passwordLength--;
   }
   if (isSpecial === true) {
     allChoices = allChoices.concat(special);
+    let maximum = special.length - 1;
+    let rnd = Math.floor(Math.random() * (maximum - 0 + 1)) + 0;
+    password.push(special[rnd]);
+    passwordLength--;
   }
   for (let i = 0; i < passwordLength; i++) {
-    let = maximum = allChoices.length - 1;
+    let maximum = allChoices.length - 1;
     let minimum = 0;
     let rnd = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
     password.push(allChoices[rnd]);
@@ -53,8 +76,11 @@ function writePassword() {
   let passwordText = document.querySelector("#password");
 
   passwordText.value = password.join("");
+  // let playAgain = confirm("Do you want to generate another password?");
+  // if (playAgain === true) {
+  //   generatePassword();
+  // }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-``
